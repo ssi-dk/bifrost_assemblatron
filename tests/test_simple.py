f937f03606b42e708e01adbcb5ec7ef08842efd9
Kim Ng <kimleeng@gmail.com>
2021-02-10 08:54:41 +0100

    Bump version: v2_2_1 → v2_2_2

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -22,7 +22,7 @@ def test_connection():
 
 
 class TestBifrostAssemblatron:
-    component_name = "assemblatron__v2_2_1"
+    component_name = "assemblatron__v2_2_2"
     current_dir = os.getcwd()
     test_dir = "/bifrost/test_data/output/test__assemblatron/"
     json_entries = [
