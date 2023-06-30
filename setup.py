1549f759236a2cd6132e5c2b0f72b205e4389b46
Kristoffer Kiil <krki@ssi.dk>
2023-06-30 09:52:02 +0200

    Bump version: 2.3.0 → 2.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_assemblatron',
-    version='2.3.0',
+    version='2.3.1',
     description='Denovo assembly component for Bifrost',
     url='https://github.com/ssi-dk/bifrost_assemblatron',
     author="Kim Ng, Martin Basterrechea",
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -42,7 +42,7 @@ def sample(use_collection):
 
 
 class TestBifrostAssemblatron:
-    component_name = "assemblatron__v2.3.0"
+    component_name = "assemblatron__v2.3.1"
     current_dir = os.getcwd()
     json_entries = [
         {
