7891bb81f22a50895d97edb1df9e73dfb3a4609d
Kristoffer Kiil <krki@ssi.dk>
2023-11-06 10:24:57 +0100

    Added mamba support to install script

--- install.sh
+++ install.sh
@@ -35,6 +35,8 @@ then
   echo "Starting local install"
 fi
 
+CONDA_CMD=${CONDACMD:-"conda"}
+
 if [ "$parameterI" == "COMP" ]
 then
   echo "Starting computerome install"
@@ -49,30 +51,30 @@ then
     exit 1
   else
     echo -e "\nAdding conda envs_dirs and pkgs_dirs"
-    echo "conda config --add envs_dirs $BIFROST_CONDA_PATH/envs"
-    echo "conda config --add pkgs_dirs $BIFROST_CONDA_PATH/pkgs"
-    conda config --add envs_dirs $BIFROST_CONDA_PATH/envs
-    conda config --add pkgs_dirs $BIFROST_CONDA_PATH/pkgs
+    echo "$CONDA_CMD config --add envs_dirs $BIFROST_CONDA_PATH/envs"
+    echo "$CONDA_CMD config --add pkgs_dirs $BIFROST_CONDA_PATH/pkgs"
+    $CONDA_CMD config --add envs_dirs $BIFROST_CONDA_PATH/envs
+    $CONDA_CMD config --add pkgs_dirs $BIFROST_CONDA_PATH/pkgs
   fi
 fi
 
 # Begin script
-if $(conda config --show channels | grep -q "bioconda")
+if $($CONDA_CMD config --show channels | grep -q "bioconda")
 then
   echo "bioconda channel is already added"
 else
-  conda config --add channels bioconda
+  $CONDA_CMD config --add channels bioconda
   echo "bioconda channel was added, you can remove it after installation with command"
-  echo "conda config --remove channels bioconda"
+  echo "$CONDA_CMD config --remove channels bioconda"
 fi
 
-if $(conda config --show channels | grep -q "conda-forge")
+if $($CONDA_CMD config --show channels | grep -q "conda-forge")
 then
   echo "conda-forge channel is already added"
 else
-  conda config --add channels conda-forge
+  $CONDA_CMD config --add channels conda-forge
   echo "bioconda channel was added, you can remove it after installation with command"
-  echo "conda config --remove channels conda-forge"
+  echo "$CONDA_CMD config --remove channels conda-forge"
 fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -101,11 +103,11 @@ fi
 
 CUSTOM_INSTALL_PATH=$(find $SCRIPT_DIR -name "custom_install.sh")
 #check if env $ENV_NAME already exists
-if $(conda env list | grep -q "$ENV_NAME")
+if $($CONDA_CMD env list | grep -q "$ENV_NAME")
 then
   echo "Environment $ENV_NAME already exists"
   echo -e "\nIf you want to update it, please remove it first"
-  echo "conda env remove --name $ENV_NAME"
+  echo "$CONDA_CMD env remove --name $ENV_NAME"
   if test -f "$CUSTOM_INSTALL_PATH"
   then
     echo -e "\nIf you want to run the custom install part, you can execute:"
@@ -119,21 +121,21 @@ if test -f "$REQ_TXT";
 then
   echo "Making conda env"
   echo "$ENV_NAME will be created"
-  conda env create --file "$REQ_TXT" --name $ENV_NAME
+  $CONDA_CMD env create --file "$REQ_TXT" --name $ENV_NAME
 else
   echo "environment.yml file cannot be found in the script folder"
   exit 1
 fi
 
-if $(conda env list | grep -q "$ENV_NAME")
+if $($CONDA_CMD env list | grep -q "$ENV_NAME")
 then
   if [ "$parameterI" == "COMP" ]
   then
     echo -e "\nRemoving conda envs_dirs and pkgs_dirs"
-    echo "conda config --remove envs_dirs $BIFROST_CONDA_PATH/envs"
-    echo "conda config --remove pkgs_dirs $BIFROST_CONDA_PATH/pkgs"
-    conda config --remove envs_dirs $BIFROST_CONDA_PATH/envs
-    conda config --remove pkgs_dirs $BIFROST_CONDA_PATH/pkgs
+    echo "$CONDA_CMD config --remove envs_dirs $BIFROST_CONDA_PATH/envs"
+    echo "$CONDA_CMD config --remove pkgs_dirs $BIFROST_CONDA_PATH/pkgs"
+    $CONDA_CMD config --remove envs_dirs $BIFROST_CONDA_PATH/envs
+    $CONDA_CMD config --remove pkgs_dirs $BIFROST_CONDA_PATH/pkgs
   fi
   echo "Environment $ENV_NAME was created"
 else
